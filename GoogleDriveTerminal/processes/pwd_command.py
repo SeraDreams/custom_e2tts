3d1e2de5a23212bcb8a84ecd1a5c96e0fc37d89c
Sera <117317257+SeraDreams@users.noreply.github.com>
2024-10-11 19:30:12 +0300

    Delete GoogleDriveTerminal directory

--- GoogleDriveTerminal/processes/pwd_command.py
+++
@@ -1,8 +0,0 @@
-from work_with_state.current_dir_writter import get_cur_dir
-
-
-# вывод полного пути текущего каталога
-def show_cur_dir_path() -> str:
-    dir_path = get_cur_dir()
-
-    return dir_path
--- GoogleDriveTerminal/processes/system_interaction.py
+++
@@ -1,7 +0,0 @@
-from os import system
-
-
-# команда системы
-def system_command_exec(command: str) -> str:
-    system(command)
-    return 'BASH: command was executed'
